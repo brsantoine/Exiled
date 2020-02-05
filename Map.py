aaec03231ae5fb6c1e17ec908b701d24e4608e6f
TheKiwy <tttthekiwy@gmail.com>
2020-02-06 00:19:51 +0100

    Map avec ennemis

--- Map.py
+++ Map.py
@@ -21,17 +21,17 @@ class Map:
 
         file = open("maps/" + fileName + ".map")
 
-        notDone = True
+        readingMap = True
 
         x = 0
         y = 0
 
-        while notDone:
+        while readingMap:
 
             line = file.readline()
 
             if not line:
-                notDone = False
+                readingMap = False
             else:
                 for lettre in line:
                     if lettre == 'W':
@@ -42,10 +42,28 @@ class Map:
                         self.moneyList.append(Money(x * TAILLE_CASE, y * TAILLE_CASE, TAILLE_CASE, TAILLE_CASE))
                     if lettre == 'S':
                         self.playerSpawn = (64 * x, 64 * y)
+                    if lettre == '#':
+                        readingMap = False
                     x += 1
                 
             y += 1
             x = 0
+
+        readingEnemies = True
+
+        while readingEnemies:
+
+            line = file.readline()
+
+            if not line:
+                readingEnemies = False
+            else:
+                enemy = line.split(" ; ")
+                if enemy[4] == "LOW":
+                    delay = ENEMY_LOWDELAY
+                else:
+                    delay = ENEMY_HIGHDELAY
+                self.enemies.append(Enemy((float(enemy[0]) * TAILLE_CASE), (float(enemy[1]) * TAILLE_CASE), (float(enemy[2]) * TAILLE_CASE), float(enemy[3]), delay, float(enemy[5])))
 
         file.close()
 
@@ -64,23 +82,6 @@ class Map:
 
         for indexToDelete in reversed(indexesToDelete):
             del self.moneyList[indexToDelete]
-
-        self.enemies.append(Enemy((6.5 * TAILLE_CASE),(1.5 * TAILLE_CASE),(6 * TAILLE_CASE),1,ENEMY_LOWDELAY,0))
-        self.enemies.append(Enemy((9 * TAILLE_CASE),(5 * TAILLE_CASE),(24 * TAILLE_CASE),0,ENEMY_LOWDELAY,0))
-        self.enemies.append(Enemy((19 * TAILLE_CASE),(2 * TAILLE_CASE),(4 * TAILLE_CASE),1,ENEMY_LOWDELAY,0))
-        self.enemies.append(Enemy((30 * TAILLE_CASE),(2 * TAILLE_CASE),(6 * TAILLE_CASE),1,ENEMY_LOWDELAY,0))
-        self.enemies.append(Enemy((44 * TAILLE_CASE),(7 * TAILLE_CASE),0,1,ENEMY_LOWDELAY,1))
-        self.enemies.append(Enemy((44 * TAILLE_CASE),(18 * TAILLE_CASE),0,1,ENEMY_LOWDELAY,2))
-        self.enemies.append(Enemy((38 * TAILLE_CASE),(15 * TAILLE_CASE),(5 * TAILLE_CASE),1,ENEMY_HIGHDELAY,0))
-        self.enemies.append(Enemy((21 * TAILLE_CASE),(11 * TAILLE_CASE),0,1,ENEMY_LOWDELAY,4))
-        self.enemies.append(Enemy((24 * TAILLE_CASE),(11 * TAILLE_CASE),0,1,ENEMY_LOWDELAY,4))
-        self.enemies.append(Enemy((7 * TAILLE_CASE),(16 * TAILLE_CASE),(6 * TAILLE_CASE),0,ENEMY_HIGHDELAY,0))
-        self.enemies.append(Enemy((12 * TAILLE_CASE),(22 * TAILLE_CASE),(14 * TAILLE_CASE),0,ENEMY_LOWDELAY,0))
-        self.enemies.append(Enemy((1.5 * TAILLE_CASE),(18.5 * TAILLE_CASE),(8 * TAILLE_CASE),1,ENEMY_HIGHDELAY,0))
-        self.enemies.append(Enemy((29 * TAILLE_CASE),(30 * TAILLE_CASE),0,1,ENEMY_LOWDELAY,3))
-        self.enemies.append(Enemy((23 * TAILLE_CASE),(30 * TAILLE_CASE),0,1,ENEMY_LOWDELAY,2))
-        self.enemies.append(Enemy((40 * TAILLE_CASE),(31 * TAILLE_CASE),(4 * TAILLE_CASE),0,ENEMY_HIGHDELAY,0))
-        self.enemies.append(Enemy((32 * TAILLE_CASE),(19 * TAILLE_CASE),(2 * TAILLE_CASE),1,ENEMY_HIGHDELAY,0))
 
     def update(self):
         enemyHitboxList = []
