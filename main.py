74a3f3a0cd59d22c9bda73dba4d1af5be9d15e06
MARQUES SANTOS Telmo <marquest@hostname.iut2.upmf-grenoble.fr>
2020-02-05 11:24:13 +0100

    Added game end

--- main.py
+++ main.py
@@ -19,27 +19,27 @@ screen = pg.display.set_mode(size)
 
 fpsClock = pg.time.Clock()
 
-joueur = Player(384, 2240, 64, 64)
+joueur = Player(MAP_START_X, MAP_START_Y, 64, 64)
 enemies = []
-enemies.append(Enemy((6.5 * TAILLE_CASE),(1.5 * TAILLE_CASE),(6 * TAILLE_CASE),1,5,0))
-enemies.append(Enemy((9 * TAILLE_CASE),(5 * TAILLE_CASE),(24 * TAILLE_CASE),0,5,0))
-enemies.append(Enemy((19 * TAILLE_CASE),(2 * TAILLE_CASE),(4 * TAILLE_CASE),1,5,0))
-enemies.append(Enemy((30 * TAILLE_CASE),(2 * TAILLE_CASE),(6 * TAILLE_CASE),1,5,0))
-enemies.append(Enemy((44 * TAILLE_CASE),(7 * TAILLE_CASE),0,1,5,1))
-enemies.append(Enemy((44 * TAILLE_CASE),(18 * TAILLE_CASE),0,1,5,2))
-enemies.append(Enemy((38 * TAILLE_CASE),(15 * TAILLE_CASE),(5 * TAILLE_CASE),1,30,0))
-enemies.append(Enemy((21 * TAILLE_CASE),(11 * TAILLE_CASE),0,1,5,4))
-enemies.append(Enemy((24 * TAILLE_CASE),(11 * TAILLE_CASE),0,1,5,4))
-enemies.append(Enemy((7 * TAILLE_CASE),(16 * TAILLE_CASE),(6 * TAILLE_CASE),0,30,0))
-enemies.append(Enemy((12 * TAILLE_CASE),(22 * TAILLE_CASE),(14 * TAILLE_CASE),0,5,0))
-enemies.append(Enemy((1.5 * TAILLE_CASE),(18.5 * TAILLE_CASE),(8 * TAILLE_CASE),1,30,0))
-enemies.append(Enemy((29 * TAILLE_CASE),(30 * TAILLE_CASE),0,1,1,3))
-enemies.append(Enemy((23 * TAILLE_CASE),(30 * TAILLE_CASE),0,1,1,2))
-enemies.append(Enemy((44 * TAILLE_CASE),(31 * TAILLE_CASE),0,1,1,1))
-enemies.append(Enemy((32 * TAILLE_CASE),(19 * TAILLE_CASE),(2 * TAILLE_CASE),1,30,0))
+enemies.append(Enemy((6.5 * TAILLE_CASE),(1.5 * TAILLE_CASE),(6 * TAILLE_CASE),1,ENEMY_LOWDELAY,0))
+enemies.append(Enemy((9 * TAILLE_CASE),(5 * TAILLE_CASE),(24 * TAILLE_CASE),0,ENEMY_LOWDELAY,0))
+enemies.append(Enemy((19 * TAILLE_CASE),(2 * TAILLE_CASE),(4 * TAILLE_CASE),1,ENEMY_LOWDELAY,0))
+enemies.append(Enemy((30 * TAILLE_CASE),(2 * TAILLE_CASE),(6 * TAILLE_CASE),1,ENEMY_LOWDELAY,0))
+enemies.append(Enemy((44 * TAILLE_CASE),(7 * TAILLE_CASE),0,1,ENEMY_LOWDELAY,1))
+enemies.append(Enemy((44 * TAILLE_CASE),(18 * TAILLE_CASE),0,1,ENEMY_LOWDELAY,2))
+enemies.append(Enemy((38 * TAILLE_CASE),(15 * TAILLE_CASE),(5 * TAILLE_CASE),1,ENEMY_HIGHDELAY,0))
+enemies.append(Enemy((21 * TAILLE_CASE),(11 * TAILLE_CASE),0,1,ENEMY_LOWDELAY,4))
+enemies.append(Enemy((24 * TAILLE_CASE),(11 * TAILLE_CASE),0,1,ENEMY_LOWDELAY,4))
+enemies.append(Enemy((7 * TAILLE_CASE),(16 * TAILLE_CASE),(6 * TAILLE_CASE),0,ENEMY_HIGHDELAY,0))
+enemies.append(Enemy((12 * TAILLE_CASE),(22 * TAILLE_CASE),(14 * TAILLE_CASE),0,ENEMY_LOWDELAY,0))
+enemies.append(Enemy((1.5 * TAILLE_CASE),(18.5 * TAILLE_CASE),(8 * TAILLE_CASE),1,ENEMY_HIGHDELAY,0))
+enemies.append(Enemy((29 * TAILLE_CASE),(30 * TAILLE_CASE),0,1,ENEMY_LOWDELAY,3))
+enemies.append(Enemy((23 * TAILLE_CASE),(30 * TAILLE_CASE),0,1,ENEMY_LOWDELAY,2))
+enemies.append(Enemy((40 * TAILLE_CASE),(31 * TAILLE_CASE),(4 * TAILLE_CASE),0,ENEMY_HIGHDELAY,0))
+enemies.append(Enemy((32 * TAILLE_CASE),(19 * TAILLE_CASE),(2 * TAILLE_CASE),1,ENEMY_HIGHDELAY,0))
 
 
-
+startRect = pg.Rect(MAP_START_X - TAILLE_CASE,MAP_START_Y + (2 * TAILLE_CASE),3 * TAILLE_CASE,1 * TAILLE_CASE)
 camera = Camera()
 
 map1 = Map("map1", None)
@@ -68,15 +68,29 @@ while True:
                 enemyHitboxList.append(hitbox)
 
         alive = joueur.update(pg.key.get_pressed(), collisionList,enemyHitboxList)
+        expeditionClear = joueur.expeditionClear(startRect)
 
         camera.draw(screen, joueur, collisionList, enemyHitboxList)
+        
+
 
         enemyHitboxList = []
-
-
         pg.display.update()
 
         if not alive:
             village.village = True
-            joueur.rect.left = 384
-            joueur.rect.top = 2240
+            joueur.rect.left = MAP_START_X
+            joueur.rect.top = MAP_START_Y
+        if expeditionClear:
+            village.village = True
+            joueur.rect.left = MAP_START_X
+            joueur.rect.top = MAP_START_Y
+            # Draw the transparent rect
+            see_through = pg.Surface((SCREEN_WIDTH - TAILLE_CASE,SCREEN_HEIGHT - TAILLE_CASE)).convert_alpha()
+            see_through.fill((140, 140, 140, 200))
+            screen.blit(see_through, (32,32))
+            # Attend que l'utilisateur clique sur le bouton continuer
+            
+
+             
+                        
