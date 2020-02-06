2802dbef45168f2df50dc2104672d695ff29a846
telmoms <telmosantosbp@hotmail.com>
2020-02-06 22:40:53 +0100

    Some touches

--- Money.py
+++ Money.py
@@ -1,15 +1,15 @@
 import pygame as pg
 
 class Money:
-    """Une classe qui correspond a un mur"""
+    """Une classe qui correspond a un Money"""
     def __init__(self, left, top, width, height):
-        """Initialise la classe Mur"""
+        """Initialise la classe Money"""
 
         self.rect = pg.Rect(left, top, width, height)
         self.img = pg.image.load("images/money.png")
 
     def draw(self, screen, x, y):
-        """Appelee a chaque tour de boucle, cette fonction affiche le mur"""
+        """Appelee a chaque tour de boucle, cette fonction affiche la piece"""
 
         GREEN = (0, 255, 0)
 
