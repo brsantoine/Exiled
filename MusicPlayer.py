21b7efdc157a1ea509e8231f0d971baa8666d9b3
telmoms <telmosantosbp@hotmail.com>
2020-02-06 22:06:00 +0100

    Added animations, changed window titles and icons, lowered music volume

--- MusicPlayer.py
+++ MusicPlayer.py
@@ -4,11 +4,11 @@ class MusicPlayer:
 
     def playMenuMusic(self):
         pg.mixer.music.load("music/aerocity_stranger.mp3")
-        pg.mixer.music.play()
+        pg.mixer.music.play(-1)
 
     def playExpeditionMusic(self):
         pg.mixer.music.load("music/cloudkicker_night.mp3")
-        pg.mixer.music.play()
+        pg.mixer.music.play(-1)
 
     def fadeOut(self):
         pg.mixer.music.fadeout(10000)
