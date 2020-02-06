ebe51d416db0457d36a640bd4718149a70ac36ba
Dvilx99 <antoinebre@orange.fr>
2020-02-06 18:47:01 +0100

    guard sprites

--- Exit.py
+++ Exit.py
@@ -1,11 +1,22 @@
 import pygame as pg
+import os
+from random import randint
+
 
 class Exit:
     def __init__(self, left, top, width, height):
         """Initialise la classe Mur"""
 
+        spriteList = []
+
+        for sprite in os.listdir("images/grassSprites"):
+            spriteList.append(sprite)
+
+        randomSprite = randint(0, len(spriteList) - 1)
+        
+        self.img = pg.image.load("images/grassSprites/" + spriteList[randomSprite])
+
         self.rect = pg.Rect(left, top, width, height)
-        self.img = pg.image.load("images/exit.png")
 
     def draw(self, screen, x, y):
         """Appelee a chaque tour de boucle, cette fonction affiche le mur"""
